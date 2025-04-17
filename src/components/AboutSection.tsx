0bc5636706ef8f4df94d01d1737f5abe96ac634d
srinath536 <Your_Email>
2025-04-17 10:03:05 +0530

    u

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../styles/AboutSection.css';
 
 const AboutSection = () => {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 // src/components/Header.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import '../styles/Header.css';
 
 const Header = () => {
